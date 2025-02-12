f3c8c34d2470c2dd685abe31da3c0cc77a554780
Chandima Ranaweera <chan4lk@gmail.com>
2025-02-12 16:56:30 +0530

    test deployment

---
+++ module-1/studio/router-tools.py
@@ -0,0 +1,67 @@
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# Tool
+def add(a: int, b: int) -> int:
+    """Add a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+# Tool
+def subtract(a: int, b: int) -> int:
+    """Subtract b from a.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a - b
+
+# Tool
+def divide(a: int, b: int) -> int:
+    """Divide a by b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
+llm = ChatOpenAI(model="gpt-4o")
+llm_with_tools = llm.bind_tools([multiply, add, subtract, divide])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply,add, subtract, divide]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
